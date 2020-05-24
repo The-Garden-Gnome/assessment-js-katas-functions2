1767992763bcf0299dbba0afbd4ad5d489ed46a6
Samantha Taylor <staylor@mort.is>
2020-05-24 17:39:17 -0400

    Power function complete.

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,19 @@
 let additionkata = document.querySelector('#addition')
 let multiplicationkata = document.querySelector('#multiplication')
+let powerkata = document.querySelector('#power')
 
 function add(a, b) {
     return a + b
   }
 
   let addition = add(2, 4)
-  additionkata.innerText = "Answer is: " + addition + ".";
+  additionkata.innerText = addition;
 
   function multiply(x,y) {
     let result = 0
      for (let i = 0; i < y; i++){
         result = add(result, x);
-        console.log(result)
+        // console.log(result)
      }
      return result
   }
@@ -20,4 +21,14 @@ function add(a, b) {
   let multiplication = multiply(4, 6)
   multiplicationkata.innerText = multiplication
 
-  
+function power(x,n) {
+    let result = 1
+    for (let i = 0; i < n; i++){
+        result = multiply(multiply(x,n), multiply(x,n))
+        // console.log(result)
+    }
+return result
+}
+
+let Exponentiation = power(2, 8)
+powerkata.innerText = Exponentiation
